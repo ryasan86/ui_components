62e05d81921c337450cf44eb15b015b4f9862449
Ryan Santos <ryansantos86@gmail.com>
2019-07-21 11:34:17 -0700

    adding pac man

--- notifications_search_menu/script.js
+++ notifications_search_menu/script.js
@@ -1,8 +1,14 @@
 const notifications = document.querySelector('.notifications-wrap');
 const menu          = document.querySelector('.menu-wrap');
 const menuIcon      = document.querySelector('.menu-icon');
+const searchIcon    = document.querySelector('.search-icon');
+const searchInput   = document.querySelector('.search-input');
 
 menuIcon.addEventListener('click', () => {
   notifications.classList.toggle('active');
-    menu.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+searchIcon.addEventListener('click', () => {
+  searchInput.classList.toggle('active');
 });
